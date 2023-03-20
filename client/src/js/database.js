6bbef9dc22eaf74000256e04f6cfcffd8f44b805
John Lai <lai.john.py@gmail.com>
2023-03-19 23:34:33 -0400

    Remove editor and header from webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,8 +19,7 @@ export const putDb = async (content) => {
     const jateDb = await openDB('jate', 1);
     const tx = jateDb.transaction('jate', 'readwrite');
     const store = tx.objectStore('jate');
-    const request = store.put({ todo: content });
-    // const request = store.put({ id: id, todo: content });
+    const request = store.put({ id: 1, value: content });
     const result = await request;
     console.log('🚀 - data saved to the database', result);
   } catch (error) {
@@ -41,12 +40,20 @@ export const getDb = async () => {
     const store = tx.objectStore('jate');
 
     // Use the .getAll() method to get all data in the database.
-    const request = store.getAll();
-
+    const request = store.get(1);
     // Get confirmation of the request.
     const result = await request;
-    console.log('result.value', result);
-    return result;
+
+    if (!result) {
+      console.log('result.value', result);
+
+      return false;
+    } else {
+      return result.value;
+    }
+
+    // console.log('result.value', result);
+    // return result;
   } catch (err) {
     console.error('getDb not implemented');
   }
